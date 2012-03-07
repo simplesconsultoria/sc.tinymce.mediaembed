3602c4163341d237da1ab68a1abecc822159628e
hvelarde <hector.velarde@gmail.com>
2012-03-07 15:17:53 -0300

    added i18n script and some empty translation files

--- rebuild_i18n.sh
+++ rebuild_i18n.sh
@@ -1,14 +1,18 @@
 #! /bin/sh
 
+PATH=bin:$PATH
 I18NDOMAIN="s17.tinymce.mediaembed"
+BASE_DIRECTORY="s17/tinymce/mediaembed"
 
 # Synchronise the templates and scripts with the .pot.
-# All on one line normally:
-i18ndude rebuild-pot --pot s17/tinymce/mediaembed/locales/${I18NDOMAIN}.pot \
+i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot \
     --create ${I18NDOMAIN} \
-   s17/tinymce/mediaembed
+    ${BASE_DIRECTORY}
 
 # Synchronise the resulting .pot with all .po files
-for po in s17/tinymce/mediaembed/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
-    i18ndude sync --pot s17/tinymce/mediaembed/locales/${I18NDOMAIN}.pot $po
+for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
+    i18ndude sync --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot $po
 done
+
+# Report of errors and suspect untranslated messages
+i18ndude find-untranslated ${BASE_DIRECTORY}
